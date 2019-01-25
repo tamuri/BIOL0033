e531f1c3a12e27972e7d7dced469384163112581
Asif Tamuri <tamuri@gmail.com>
2019-01-25 19:33:58 +0000

    wip

--- practical_1.R
+++ practical_1.R
@@ -3,10 +3,16 @@
 
 setwd("~/Documents/2019/biol0030/BIOL0033")
 
-# ---------- BASIC PHYLOGENY PLOTTING -----------
+# ---------- LOAD ALL THE REQUIRED PACKAGES ----------
 
-# Load the 'ape' phylogenetics package
+# 'ape' and 'phangorn' provide vast array of phylogenetic analyses
 library(ape)
+library(phangorn)
+
+# 'msa' provides multiple sequence alignment
+library(msa)
+
+# ---------- BASIC PHYLOGENY PLOTTING -----------
 
 # Trees are typically stored sing the Newick format
 my_newick <- '(a, (b, c));'
@@ -71,15 +77,12 @@ plot(mammals_72sp, type='phylogram', use.edge.length=FALSE)
 nodelabels("Carnivora", PUT-NODE-NUMBER)
 nodelabels("Glires", PUT-NODE-NUMBER)
 
+
 # ---------- MULTIPLE SEQUENCE ALIGNMENT ----------
 # Align sequences using the 'msa' package
 
-# Load the required package
-library(msa)
-
 # Load the unaligned sequences
 sequences <- readDNAStringSet('NADH6.fas', format='fasta')
-# sequences <- readAAStringSet('~/Documents/2018/tree_annotator/etc/examples/flu.pb2.aminoacids/PB2.fasta', format='fasta')
 
 # The msa package provides several alignment tools. By default it uses the 
 # program "ClustalW". To align the sequences using the default options
@@ -94,11 +97,14 @@ print(aligned_sequences, show='complete')
 # Save the multiple sequence alignment
 writeXStringSet(unmasked(aligned_sequences), file='NADH6_aligned.fasta')
 
-# More:
-# 
-# * Try two other algorithms: ClustalOmega and MUSCLE
-#  `nadh6_alignment_muscle <- msa(nadh6_sequences, "Muscle")`
-# * Options for the Muscle aligner: gapOpen, gapExtend
+# Try some variations below. For each, save the MSA to a different file and compare in AliView
+
+# Try a different algorithm: ClustalOmega or MUSCLE
+# e.g. aligned_sequences <- msa(aligned_sequences, "Muscle")`
+
+# Try varying the gapOpening and gapExtension values
+# e.g. aligned_sequences <- msa(aligned_sequences, "Muscle", gapOpening=0, gapExtension=0)
+# e.g. aligned_sequences <- msa(aligned_sequences, "Muscle", gapOpening=1000, gapExtension=1000)
 
 
 # ---------- VISUALISE TREES ----------
@@ -112,13 +118,13 @@ writeXStringSet(unmasked(aligned_sequences), file='NADH6_aligned.fasta')
 # Load the package
 library(ape)
 
-# Convert our multiple sequence alignment into the format required by ape:
-
-# TODO: this doesn't work
-# aligned_sequences <- msaConvert(aligned_sequences, type='ape::DNAbin')
-
 # Read the aligned sequences we had saved to file above
-aligned_sequences <- read.FASTA('NADH6_aligned.fasta', type='DNA')
+aligned_sequences <- read.dna('NADH6_aligned.fasta', format='fasta')
+
+# Lets see what the alignment looks like
+checkAlignment(aligned_sequences)
+
+# What kind of things should we look for?
 
 # Calculate pairwise distances between sequences
 distances <- dist.dna(aligned_sequences, model='JC')
@@ -126,9 +132,21 @@ distances <- dist.dna(aligned_sequences, model='JC')
 # Look at the distance matrix
 distances
 
-# Visualise the distances (requires loading the 'ade4' library)
-library(ade4)
 
+# ---------- CREATE A UPGMA TREE USING DISTANCES ----------
+
+upgma_tree <- upgma(distances)
+upgma_tree <- ladderize(root(upgma_tree, outgroup=c('pig', 'cow'), resolve.root = TRUE))
+plot(upgma_tree)
+
+# look at the branch lengths
+upgma_tree$edge.length
+
+# look at the newick representation
+write.tree(upgma_tree)
+
+# save the tree to file
+write.tree(upgma_tree, file="nadh6_upgma.tree")
 
 # ---------- CREATE A NEIGHBOUR-JOINING TREE USING DISTANCES ----------
 
@@ -136,7 +154,7 @@ library(ade4)
 nj_tree <- nj(distances)
 
 # View the tree
-plot(ladderize(nj_tree))
+plot(nj_tree)
 add.scale.bar()
 
 # By default the tree is unrooted. Even if it looks OK, _always_ explicitly root the tree
@@ -145,18 +163,36 @@ nj_tree <- ladderize(nj_tree)
 plot(nj_tree)
 add.scale.bar()
 
-# Plot the rooted tree. how does it look different?
+# To save a tree
+write.tree(nj_tree, file = 'nadh6_nj.tree')
 
-# ---------- ESTIMATE TREE BY MAXIMUM LIKELIHOOD ----------
 
-# The 'phangorn' library provides several methods of pylogeny estimation
-library(phangorn)
+# ---------- ESTIMATE TREE BY MAXIMUM PARSIMONY ----------
 
 # Convert the aligned sequences loaded using 'ape' into the format required by phangorn
 aligned_sequences_pd <- as.phyDat(aligned_sequences)
 
+random_starting_tree <- random.addition(aligned_sequences_pd)
+
+# calculate the parsimony score for the NJ tree
+parsimony(random_starting_tree, aligned_sequences_pd)
+
+# optimise the tree using maximm parsimony
+pars_tree <- optim.parsimony(random_starting_tree, aligned_sequences_pd)
+pars_tree <- ladderize(root(pars_tree, outgroup=c('cow', 'pig'), resolve.root=TRUE))
+plot(pars_tree)
+
+# what's missing? look at the newick representation
+write.tree(pars_tree)
+
+# we can look at the changes on the tree for any given site. look at the 12th site
+anc.pars <- ancestral.pars(pars_tree, aligned_sequences_pd)
+plotAnc(pars_tree, anc.pars, attr(anc.pars, "index")[12])
+
+# ---------- ESTIMATE TREE BY MAXIMUM LIKELIHOOD ----------
+
 # Get an initial tree to optimise
-tre.ini <- nj(dist.dna(aligned_sequences, model = "TN93"))
+tre.ini <- nj_tree
 
 # Package up the tree, alignment and model information into a likelihhod
 fit.ini <- pml(tre.ini, aligned_sequences_pd, k = 4)
@@ -168,8 +204,7 @@ fit <- optim.pml(fit.ini, optNni = TRUE, optBf = TRUE, optQ = TRUE, optGamma = T
 ml.tree <- fit$tree
 
 # Root the ML tree using the outgroup species
-ml.tree <- root(ml.tree, outgroup=c('pig', 'cow'))
-ml.tree <- ladderize(ml.tree)
+ml.tree <- ladderize(root(ml.tree, outgroup=c('pig', 'cow'), resolve.root=T))
 
 # Plot the ML tree
 plot(ml.tree)
@@ -187,8 +222,11 @@ par(mfrow=c(2,1), mai=c(0.2,0.2,0.2,0.2))
 
 # Plot the NJ tree, then the ML tree and add a scale bar
 plot(nj_tree)
+edgelabels(round(nj_tree$edge.length, digits=3))
 add.scale.bar()
+
 plot(ml.tree)
+edgelabels(round(ml.tree$edge.length, digits=3))
 add.scale.bar()
 
 # Calculate the Robinson-Foulds distance between the two trees
@@ -200,45 +238,24 @@ comparePhylo(nj_tree, ml.tree, plot=TRUE)
 
 # ----------- LARGER EXAMPLE ----------
 
-library(msa)
-library(ape)
-library(phangorn)
-
-# read the unaligned sequences
-sequences <- readDNAStringSet('NADH6.fas', format='fasta')
-
-# try a few different alignment strategies (check in aliview - how do they differ?)
-aligned_sequences <- msa(sequences, "Muscle", verbose = TRUE)
-writeXStringSet(unmasked(aligned_sequences), file='NADH6.aln1.fasta')
-
-aligned_sequences <- msa(sequences, "Muscle", verbose = TRUE, gapOpening = 10, gapExtension = 10)
-writeXStringSet(unmasked(aligned_sequences), file='NADH6.aln2.fasta')
-
-aligned_sequences <- msa(sequences, "Muscle", verbose = TRUE, gapOpening = 0, gapExtension = 0)
-writeXStringSet(unmasked(aligned_sequences), file='NADH6.aln3.fasta')
+# Compare mammal trees for two mitochondrial genes
 
 # estimate a neighbour joining tree
 
-aln1 <- read.FASTA('NADH6.aln1.fasta', type='DNA')
-aln2 <- read.FASTA('NADH6.aln2.fasta', type='DNA')
-aln3 <- read.FASTA('NADH6.aln3.fasta', type='DNA')
+cyb_aln <- read.dna('CYB.aln.ffn', format='fasta')
+cyb_dist <- dist.dna(cyb_aln)
+cyb_tree <- nj(cyb_dist)
+cyb_tree <- ladderize(root(cyb_tree, outgroup='sus_scrofa', resolve.root=TRUE))
 
-dist1 <- dist.dna(aln1)
-dist2 <- dist.dna(aln2)
-dist3 <- dist.dna(aln3)
+nd6_aln <- read.dna('ND6.aln.ffn', format='fasta')
+nd6_dist <- dist.dna(nd6_aln)
+nd6_tree <- nj(nd6_dist)
+nd6_tree <- ladderize(root(nd6_tree, outgroup='sus_scrofa', resolve.root=TRUE))
 
-njtree1 <- njs(dist1)
-njtree2 <- njs(dist2)
-njtree3 <- njs(dist3)
+comparePhylo(cyb_tree, nd6_tree, plot=TRUE)
 
-plot(njtree3)
-add.scale.bar()
-
-njtree1 <- ladderize(root(njtree1, outgroup=c('pig', 'cow'), resolve.root = TRUE))
-njtree2 <- ladderize(root(njtree2, outgroup=c('pig', 'cow'), resolve.root = TRUE))
-njtree3 <- ladderize(root(njtree3, outgroup=c('pig', 'cow'), resolve.root = TRUE))
-
-comparePhylo(njtree1, njtree2, plot=TRUE)
+association <- cbind(cyb_tree$tip.label, nd6_tree$tip.label)
+cophyloplot(cyb_tree, nd6_tree, assoc = association)
 
 aligned_sequences_pd <- as.phyDat(aln1)
 tre.ini <- nj(dist.dna(aln1, model = "TN93"))
@@ -252,3 +269,49 @@ add.scale.bar()
 
 comparePhylo(njtree2, ml.tree, plot=TRUE)
 
+otuPhylo <- function(phy, cutoff=0.05) {
+  
+  dists <- cophenetic(phy)
+  diag(dists) <- NA
+  pruned <- vector()
+  
+  for (i in phy$tip.label) {
+    if (!(i %in% pruned)) {
+      d <- na.omit(dists[i,])
+      pruned <- c(pruned, names(which(d <= cutoff)))
+    }
+  }
+  
+  if (length(pruned) > 0) {
+    print("Dropping the following taxa:")
+    print(pruned)
+    return(drop.tip(phy, pruned))
+  }
+  else {
+    return(phy)
+  }
+  
+}
+
+
+
+drop <- cyb_tree$tip.label[!(cyb_tree$tip.label %in% apes)]
+cyb_tree <- drop.tip(cyb_tree, drop)
+
+
+
+
+# get subset of sequences
+cyb_aln <- cyb_aln[which(labels(cyb_aln) %in% apes),]
+nd6_aln <- nd6_aln[which(labels(nd6_aln) %in% apes),]
+
+cyb_tree <- otuPhylo(cyb_tree, 0.15)
+
+drop <- nd6_tree$tip.label[!(nd6_tree$tip.label %in% cyb_tree$tip.label)]
+nd6_tree <- drop.tip(nd6_tree, drop)
+
+cyb_aln <- cyb_aln[which(labels(cyb_aln) %in% cyb_tree$tip.label),]
+nd6_aln <- nd6_aln[which(labels(nd6_aln) %in% cyb_tree$tip.label),]
+
+write.FASTA(cyb_aln, file='cyb.30prim.aln.fasta')
+write.FASTA(nd6_aln, file='nd6.30prim.aln.fasta')
