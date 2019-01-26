18ab6d7ff3bacea263d9e5899cbbaa0f5648722e
Asif Tamuri <tamuri@gmail.com>
2019-01-26 14:37:35 +0000

    cleaner, shorter, var names

--- practical_1.R
+++ practical_1.R
@@ -82,29 +82,29 @@ nodelabels("Glires", 79)
 # Align sequences using the 'msa' package
 
 # Load the unaligned sequences
-sequences <- readDNAStringSet('nadh6.8apes.fasta', format='fasta')
+seqs <- readDNAStringSet('nadh6.8apes.fasta', format='fasta')
 
 # The msa package provides several alignment tools. By default it uses the 
 # program "ClustalW". To align the sequences using the default options
-aligned_sequences <- msa(sequences)
+aln <- msa(seqs)
 
 # Take a look at the alignment
-aligned_sequences
+aln
 
 # Look at the entire alignment, not truncated
-print(aligned_sequences, show='complete')
+print(aln, show='complete')
 
 # Save the multiple sequence alignment
-writeXStringSet(unmasked(aligned_sequences), file='nadh6.8apes.aln.fasta')
+writeXStringSet(unmasked(aln), file='nadh6.8apes.aln.fasta')
 
 # Try some variations below. For each, save the MSA to a different file and compare in AliView
 
 # Try a different algorithm: ClustalOmega or MUSCLE
-# e.g. aligned_sequences <- msa(aligned_sequences, "Muscle")`
+# e.g. aln <- msa(seqs, "Muscle")`
 
 # Try varying the gapOpening and gapExtension values
-# e.g. aligned_sequences <- msa(aligned_sequences, "Muscle", gapOpening=0, gapExtension=0)
-# e.g. aligned_sequences <- msa(aligned_sequences, "Muscle", gapOpening=1000, gapExtension=1000)
+# e.g. aln <- msa(seqs, "Muscle", gapOpening=0, gapExtension=0)
+# e.g. aln <- msa(seqs, "Muscle", gapOpening=1000, gapExtension=1000)
 
 
 # ---------- VISUALISE TREES ----------
@@ -114,29 +114,29 @@ writeXStringSet(unmasked(aligned_sequences), file='nadh6.8apes.aln.fasta')
 # ---------- CALCULATE PAIRWISE DISTANCES ----------
 
 # The 'ape' library provides many tools for phylogenetic analysis. 
-  
-# Load the package
-library(ape)
 
 # Read the aligned sequences we had saved to file above
-aligned_sequences <- read.dna('nadh6.8apes.aln.fasta', format='fasta')
+aln <- read.dna('nadh6.8apes.aln.fasta', format='fasta')
 
 # Lets see what the alignment looks like
-checkAlignment(aligned_sequences)
+checkAlignment(aln)
 
-# What kind of things should we look for?
+# What kind of things should we look out for?
 
 # Calculate pairwise distances between sequences
-distances <- dist.dna(aligned_sequences, model='JC')
+D <- dist.dna(aln, model='JC')
 
 # Look at the distance matrix
-distances
+D
 
 
 # ---------- CREATE A UPGMA TREE USING DISTANCES ----------
 
-upgma_tree <- upgma(distances)
-upgma_tree <- ladderize(root(upgma_tree, outgroup=c('pig', 'cow'), resolve.root = TRUE))
+# setup the outgroup for the sequences
+outg <- c('pig', 'cow')
+
+upgma_tree <- upgma(D)
+upgma_tree <- ladderize(root(upgma_tree, outgroup=outg, resolve.root = TRUE))
 plot(upgma_tree)
 
 # look at the branch lengths
@@ -151,14 +151,14 @@ write.tree(upgma_tree, file="nadh6_upgma.tree")
 # ---------- CREATE A NEIGHBOUR-JOINING TREE USING DISTANCES ----------
 
 # Use the distances we calculated above
-nj_tree <- nj(distances)
+nj_tree <- nj(D)
 
 # View the tree
 plot(nj_tree)
 add.scale.bar()
 
 # By default the tree is unrooted. Even if it looks OK, _always_ explicitly root the tree
-rooted_nj <- ladderize(root(nj_tree, outgroup=c('pig', 'cow'), resolve.root=TRUE))
+rooted_nj <- ladderize(root(nj_tree, outgroup=outg, resolve.root=TRUE))
 plot(rooted_nj)
 add.scale.bar()
 
@@ -168,47 +168,47 @@ write.tree(nj_tree, file = 'nadh6_nj.tree')
 
 # ---------- ESTIMATE TREE BY MAXIMUM PARSIMONY ----------
 
-# Convert the aligned sequences loaded using 'ape' into the format required by phangorn
-aligned_sequences_pd <- as.phyDat(aligned_sequences)
+# Use the phangorn library
+aln <- read.phyDat('nadh6.8apes.aln.fasta', format = "fasta")
 
-random_starting_tree <- random.addition(aligned_sequences_pd)
+rnd_tree <- random.addition(aln)
+plot(rnd_tree)
 
-# calculate the parsimony score for the NJ tree
-parsimony(random_starting_tree, aligned_sequences_pd)
+# the parsimony score for the random starting tree
+parsimony(rnd_tree, aln)
 
-# optimise the tree using maximm parsimony
-pars_tree <- optim.parsimony(random_starting_tree, aligned_sequences_pd)
-pars_tree <- ladderize(root(pars_tree, outgroup=c('cow', 'pig'), resolve.root=TRUE))
+# optimise the tree using maximum parsimony
+pars_tree <- optim.parsimony(rnd_tree, aln)
+pars_tree <- ladderize(root(pars_tree, outgroup=outg, resolve.root=TRUE))
 plot(pars_tree)
 
 # what's missing? look at the newick representation
 write.tree(pars_tree)
 
 # we can look at the changes on the tree for any given site. look at the 12th site
-anc.pars <- ancestral.pars(pars_tree, aligned_sequences_pd)
+anc.pars <- ancestral.pars(pars_tree, aln)
 plotAnc(pars_tree, anc.pars, attr(anc.pars, "index")[12])
 
 # ---------- ESTIMATE TREE BY MAXIMUM LIKELIHOOD ----------
 
-# Get an initial tree to optimise
-tre.ini <- nj_tree
-
-# Package up the tree, alignment and model information into a likelihhod
-fit.ini <- pml(tre.ini, aligned_sequences_pd, k = 4)
+# Package up the start tree (nj tree), alignment and model information into a likelihhod
+fit_ini <- pml(nj_tree, aln, k = 4)
 
 # Optimise the tree+alignment+model using maximum likelihood (ML)
-fit <- optim.pml(fit.ini, optNni = TRUE, optBf = TRUE, optQ = TRUE, optGamma = TRUE)
+fit <- optim.pml(fit_ini, optNni = TRUE, optBf = TRUE, optQ = TRUE, optGamma = TRUE)
 
 # Get the ML tree
-ml.tree <- fit$tree
+ml_tree <- fit$tree
 
 # Root the ML tree using the outgroup species
-ml.tree <- ladderize(root(ml.tree, outgroup=c('pig', 'cow'), resolve.root=T))
+ml_tree <- ladderize(root(ml_tree, outgroup=outg, resolve.root=T))
 
 # Plot the ML tree
-plot(ml.tree)
+plot(ml_tree)
 add.scale.bar()
 
+# Maybe a bit clearer without the outgroup
+plot(drop.tip(ml_tree, outg))
 
 
 # ---------- COMPARING TREES ----------
@@ -224,70 +224,73 @@ plot(rooted_nj)
 edgelabels(round(rooted_nj$edge.length, digits=3))
 add.scale.bar()
 
-plot(ml.tree)
-edgelabels(round(ml.tree$edge.length, digits=3))
+plot(ml_tree)
+edgelabels(round(ml_tree$edge.length, digits=3))
 add.scale.bar()
 
 # Calculate the Robinson-Foulds distance between the two trees
 # If they are identical the RF distance is 0
-rf_dist <- RF.dist(nj_tree, ml.tree)
+RF.dist(pars_tree, ml_tree)
+RF.dist(nj_tree, ml_tree)
 
 # Compare the phylogenies visually
-comparePhylo(nj_tree, ml.tree, plot=TRUE)
+comparePhylo(nj_tree, ml_tree, plot=TRUE)
 
 # ----------- LARGER EXAMPLE ----------
 
 # Compare trees for two mitochondrial genes
 
+outg <- 'sus_scrofa'
+
 # cyb msa
-cyb_seqs <- readDNAStringSet('cyb.30prim.fasta', format='fasta')
-cyb_aln <- msa(cyb_seqs)
-writeXStringSet(unmasked(cyb_aln), file='cyb.30prim.aln.fasta')
+seqs1 <- readDNAStringSet('cyb.30prim.fasta', format='fasta')
+aln1 <- msa(seqs1)
+writeXStringSet(unmasked(aln1), file='cyb.30prim.aln.fasta')
 
 # estimate a neighbour joining tree for cyb
-cyb_aln <- read.dna('cyb.30prim.aln.fasta', format='fasta')
-cyb_dist <- dist.dna(cyb_aln)
-cyb_tree <- nj(cyb_dist)
-cyb_tree <- ladderize(root(cyb_tree, outgroup='sus_scrofa', resolve.root=TRUE))
+aln1 <- read.dna('cyb.30prim.aln.fasta', format='fasta')
+dist1 <- dist.dna(aln1)
+tree1 <- nj(dist1)
+tree1 <- ladderize(root(tree1, outgroup=outg, resolve.root=TRUE))
 
 # nd6 msa
-nd6_seqs <- readDNAStringSet('nd6.30prim.fasta', format='fasta')
-nd6_seqs <- msa(nd6_seqs)
-writeXStringSet(unmasked(nd6_seqs), file='nd6.30prim.aln.fasta')
+seqs2 <- readDNAStringSet('nd6.30prim.fasta', format='fasta')
+aln2 <- msa(seqs2)
+writeXStringSet(unmasked(aln2), file='nd6.30prim.aln.fasta')
 
 # estimate a nj tree for nd6
-nd6_aln <- read.dna('nd6.30prim.aln.fasta', format='fasta')
-nd6_dist <- dist.dna(nd6_aln)
-nd6_tree <- nj(nd6_dist)
-nd6_tree <- ladderize(root(nd6_tree, outgroup='sus_scrofa', resolve.root=TRUE))
+aln2 <- read.dna('nd6.30prim.aln.fasta', format='fasta')
+dist2 <- dist.dna(aln2)
+tree2 <- nj(dist2)
+tree2 <- ladderize(root(tree2, outgroup=outg, resolve.root=TRUE))
 
 # compare the two trees
-comparePhylo(cyb_tree, nd6_tree, plot=TRUE)
+comparePhylo(tree1, tree2, plot=TRUE)
 
-association <- cbind(cyb_tree$tip.label, nd6_tree$tip.label)
-cophyloplot(cyb_tree, nd6_tree, assoc = association)
+association <- cbind(tree1$tip.label, tree2$tip.label)
+cophyloplot(tree1, tree2, assoc = association)
 
 # estimate cyb tree using maximum likelihood
-aligned_sequences_pd <- as.phyDat(cyb_aln)
-tre.ini <- nj(dist.dna(cyb_aln, model = "TN93"))
-fit.ini <- pml(tre.ini, aligned_sequences_pd, k = 4)
-fit <- optim.pml(fit.ini, optNni = TRUE, optBf = TRUE, optQ = TRUE, optGamma = TRUE)
-ml.tree <- fit$tree
-ml.tree <- root(ml.tree, outgroup=c('sus_scrofa'), resolve.root=TRUE)
-ml.tree <- ladderize(ml.tree)
+aln <- read.phyDat('cyb.30prim.aln.fasta', format='fasta')
+fit_ini <- pml(tree1, aln, k = 4)
+fit <- optim.pml(fit_ini, optNni = TRUE, optBf = TRUE, optQ = TRUE, optGamma = TRUE)
+ml_tree <- ladderize(root(fit$tree, outgroup=outg, resolve.root=TRUE))
 
 # compare with cyb nj tree
-comparePhylo(cyb_tree, ml.tree, plot=TRUE)
+comparePhylo(tree1, ml_tree, plot=TRUE)
 
 # plot one tree on top of the other
 par(mfrow=c(2,1), mai=c(0.2,0.2,0.2,0.2))
-plot(cyb_tree)
+plot(tree1)
 add.scale.bar()
-plot(ml.tree)
+plot(ml_tree)
 add.scale.bar()
 
-association <- cbind(cyb_tree$tip.label, ml.tree$tip.label)
-cophyloplot(cyb_tree, ml.tree, assoc = association)
+association <- cbind(tree1$tip.label, ml_tree$tip.label)
+cophyloplot(tree1, ml_tree, assoc = association)
 
-
-
+# compare the distances between tree
+d_tree1 <- cophenetic(tree1)
+d_ml_tree <- cophenetic(ml_tree)
+plot(d_tree1, d_ml_tree)
+abline(0,1)
