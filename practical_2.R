88a86c33fd81310dfbb2ec3ef534c90ccc0a1a3a
Sandra AC <sandra.ac93@gmail.com>
2022-02-07 12:10:54 +0000

    Update R script - rstudioapi, seed num, bs vals
    - Add section that explains how to use `rstudioapi::getActiveDocumentContext()` to automatically find the path to the R script. Then, show how to use the output character vector to extract the path to the working directory. 
    - Introduce the concept of seed numbers and show how to use them when generating the random tree. 
    - Use `ape::drawSupportOnEdges`  when plotting the best-scoring ML tree.

--- practical_2.R
+++ practical_2.R
@@ -7,43 +7,68 @@
 # 2. Extract the files
 
 # ---------- SET THE WORKING DIRECTORY ----------
-# i.e. the place where you put the files for the practical
 
+# This is the place where you put the files for the practical,
+# so you should modify the path within brackets below with 
+# the path to the directory where you have these files on 
+# your PC
 setwd( '~/Documents/2019/BIOL0033' )
+
+# Alternatively, you can also automatically find the path
+# to the R script you are using if you load a specific 
+# function from the `rstudioapi` library. Follow the 
+# instructions below:
+#
+# 1. Load the package `rstudioapi`. If you do not have 
+#    it installed, then uncomment and run the
+#    command below
+# install.packages( "rstudioapi" )
+library( rstudioapi ) 
+# 2. Get the path to current open R script
+path_to_file <- rstudioapi::getActiveDocumentContext()$path
+# 3. Now just keep the path to the directory where the 
+#    R script is
+wd           <- paste( dirname( path_to_file ), "/", sep = "" )
+# 4. Set the working directory
+setwd( wd )
 
 # ---------- LOAD THE REQUIRED PACKAGES ----------
 
-# 'ape' and 'phangorn' provide vast array of phylogenetic analyses
+# `ape` and `phangorn` provide vast array of phylogenetic analyses
 library( ape )
 library( phangorn )
 
-# set the outgroup for our sequences
-outg <- c('cow', 'pig')
+# Set the outgroup for our sequences
+outg <- c( 'cow', 'pig' )
 
 # ---------- ESTIMATE TREE BY MAXIMUM PARSIMONY ----------
 
-# Use the phangorn library to read the alignment from practical 1
+# Use the `phangorn` library to read the alignment you generated 
+# in practical 1
 aln <- phangorn::read.phyDat( file = 'nadh6.8apes.aln.fasta', format = 'fasta' )
 
 # Use a random starting tree
+# HINT: You might want to set a seed number before generating the random 
+# tree in case you want to reproduce the same results!
+set.seed( 12345 )
 rnd_tree <- ape::rtree( n = length( aln ), tip.label = names( aln ) )
 ape::plot.phylo( rnd_tree )
 
 # Q: What's the parsimony score for your starting tree?
 phangorn::parsimony( tree = rnd_tree, data = aln )
 
-# optimise the tree using maximum parsimony
-pars_tree <- phangorn::optim.parsimony( tree = rnd_tree, data = aln ) # 14 nni
-pars_tree <- ape::ladderize( ape::root( phy = pars_tree, outgroup = outg, resolve.root = TRUE ) )
+# Optimise the tree using maximum parsimony
+pars_tree_unr <- phangorn::optim.parsimony( tree = rnd_tree, data = aln )
+pars_tree     <- ape::ladderize( ape::root( phy = pars_tree_unr, outgroup = outg, resolve.root = TRUE ) )
 ape::plot.phylo( pars_tree )
 
 # Q: What's the parsimony score of the maximum parsimony tree?
 # Q: How many operations did it take to find the MP tree?
-
-# what's missing? look at the newick representation
+# Q: What's missing? Look at the Newick representation
 write.tree( pars_tree )
 
-# we can look at the changes on the tree for any given site. look at the 12th site
+# We can look at the changes on the tree for any given site.
+# We will be looking at the 12th site!
 anc_pars <- phangorn::ancestral.pars( tree = pars_tree, data = aln )
 phangorn::plotAnc( tree = pars_tree, data = anc_pars, i = attr( x = anc_pars, which = 'index' )[12],
                    col = c( "green", "blue", "black", "red" ) )
@@ -92,14 +117,13 @@ ape::add.scale.bar()
 #    (Chi-sq with 1 degree of freedom has critical values of 5%: 3.84; 1%: 6.63)
 #
 # 3. Try a more complex model and answer the following.
-#
-#    e.g. try HKY+G (transition/transversion bias, unequal base frequencies, site rate variation)
+#    E.g., try HKY+G (transition/transversion bias, unequal base frequencies, site rate variation)
 #    
-#    create a new initial likelihood object that includes the gamma rate categories:
+#    Now, create a new initial likelihood object that includes the gamma rate categories:
 #
 #        fit_ini <- pml(nj(dist.hamming(aln)), aln, model='HKY', k=4)
 #
-#    then
+#    Then, run the following:
 #
 #        optim.pml(fit_ini, optNni=TRUE, model='HKY', optGamma=TRUE)
 #
@@ -138,16 +162,16 @@ ape::add.scale.bar()
 #                               on the tree, which should not be printed.
 #                               Use command below:
 ape::drawSupportOnEdges( value = boots$BP )
-# SAC-210226: It is also recommended that node labels should be added
+# SAC-210226: It is also recommended that node labels are added
 # before rooting the tree. Therefore:
 nj_tree_unr            <- ape::nj( X = ape::dist.dna( x = aln ) )
 nj_tree_unr$node.label <- boots$BP
 nj_tree_bs             <- ape::root( phy = nj_tree, outgroup = outg,
-                                    edgelabel = TRUE )
+                                     edgelabel = TRUE )
 ape::plot.phylo( x = nj_tree_bs, main='nj tree w/ bootstrap support' )
 ape::drawSupportOnEdges( value = nj_tree_bs$node.label )
 
-# HINT: Save the tree and look at it in Figtree. It might be clearer.
+# HINT: Save the tree and look at it in `Figtree` - it might be clearer!
 nj_tree$node.label <- boots$BP
 ape::write.tree( phy = nj_tree, file='nadh6.8apes.bs.tree')
 
@@ -174,7 +198,7 @@ aln <- ape::read.dna( file = 'nadh6.8apes.aln.fasta', format = 'fasta' )
 
 # A function to estimate the ML tree for this alignment
 # See section 'ESTIMATE TREE BY MAXIMUM LIKELIHOOD' above for details
-# (model is JC in this example but you can change)
+# (model is JC in this example, but you can change)
 get_ml_tree <- function(x) {
   x_pd    <- phangorn::as.phyDat( x = x )
   fit_ini <- phangorn::pml( tree = ape::nj( phangorn::dist.hamming( x = x_pd ) ),
@@ -183,7 +207,7 @@ get_ml_tree <- function(x) {
   return ( fit$tree )
 }
 
-# Run the function the get the tree for the real data
+# Run the function to get the tree for the real data
 ml_tree <- get_ml_tree( x = aln )
 
 # Look at the tree
@@ -191,8 +215,9 @@ ape::plot.phylo( x = ml_tree )
 
 # Calculate the bootstrap support for each split in the tree.
 # The `boot.phylo` function is provided by the ape package.
-# The function pass to `FUN` should be exactly what you did on the real data to estimate the tree
-# Here, we pass the `get_ml_tree` function we defined above
+# The function passed to `FUN` should be exactly what you did
+# on the real data to estimate the tree.
+# Here, we pass the `get_ml_tree` function we defined above:
 boots <- ape::boot.phylo( phy = ml_tree,
                           x = aln,
                           FUN = function( bs_aln ) get_ml_tree( x = bs_aln ),
@@ -201,9 +226,12 @@ boots <- ape::boot.phylo( phy = ml_tree,
 # Plot the tree with bootstrap support
 ape::plot.phylo( x = ml_tree, main = 'ml tree w/ bootstrap support' )
 ape::add.scale.bar()
-ape::nodelabels( boots$BP )
+# SAC-220207: Not used anymore as this prints an extra bootstrap value for the root 
+#             on the tree, which should not be printed.
+#ape::nodelabels( boots$BP )
+ape::drawSupportOnEdges( boots$BP )
 
-# HINT: Save the tree and look at it in Figtree. It might be clearer.
+# Save the tree with BS values and look at it in `Figtree`.
 ml_tree$node.label <- boots$BP
 ape::write.tree( phy = ml_tree, file = 'nadh6.8apes.ml.bs.tree' )
 
@@ -228,13 +256,14 @@ H1_opt <- phangorn::optim.pml( object = H1_ini, optNni = TRUE,
 
 # EXERCISE 4:
 #
-# Perform the likelihood ratio test
+# Perform the likelihood ratio test and answer the questions below:
 # 
 # 1. How do we calculate the test statistic? What is the value?
 #
 # 2. Calculate the degrees of freedom between the simpler and more complex model.
 #
-# 3. What is the P value given the value of test statistic? (use pchisq)
+# 3. What is the p-value given the value of test statistic?
+#    HINT: you can use `pchisq`!
 #
 # 4. Do we accept or reject the hypothesis of the molecular clock?
 
